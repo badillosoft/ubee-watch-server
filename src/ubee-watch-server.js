2a3f0b6f0206e95faac95b25d4285de72a68867c
Alan Badillo Salas <badillo.soft@hotmail.com>
2019-05-17 14:46:40 -0500

    v1.0 rev7

---
+++ src/ubee-watch-server.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const express = require("express");
+const socket_io = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+
+app.get("/credits", (req, res) => {
+    res.send({
+        author: "Alan Badillo Salas",
+        email: "badillo.soft@hotmail.com",
+        github: "https://github.com/badillosoft"
+    });
+});
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = socket_io(server);
+
+console.log("Ubee Watch v1.0");
+
+const prompt = () => `Ubee Watch [${new Date().toLocaleString()}] @`;
+
+const $host = process.env.HOST || process.argv[4] || "localhost";
+const $port = process.env.PORT || process.argv[2] || 5000;
+const $token = process.argv[3] || "ubee-watch-public-server";
+
+const $handler = `(channel, data, device = null) => socket.emit("@mode", @protocol)`;
+
+//{ app: @app, device, channel, data }
+
+io.on("connection", socket => {
+    socket.on("watcher", (watcher, callback) => {
+        
+    });
+});
+
+server.listen($port, $host, () => {
+    console.log(`${prompt()} Server started at http://${$host}:${$port}`);
+    console.log(`${prompt()} Acess token: ${$token}`);
+});
